97f19dc759ae82c418985e74e62eb5741ae9250d
luisgallegos23 <82685610+luisgallegos23@users.noreply.github.com>
2022-07-09 21:56:28 -0500

    Changed button --> input/button

---
+++ actions.js
@@ -0,0 +1,10 @@
+function showBoard(){
+    var board = document.getElementById("board");
+    var resetButton = document.getElementById("reset");
+    var instructions = document.getElementById("instructions");
+    var playButton = document.getElementById("play");
+    board.style.display('none');
+    resetButton.style.display('none');
+    instructions.style.display('none');
+    playButton.style.display('none');
+}
