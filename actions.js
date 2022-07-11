8cf3e5e949c08601d422a1d1dd7dcc5256e96e62
luisgallegos23 <82685610+luisgallegos23@users.noreply.github.com>
2022-07-11 18:35:33 -0500

    Added X O acitons

--- actions.js
+++ actions.js
@@ -1,7 +1,27 @@
+var player = 1;
+
+window.onload = function (){
+    document.getElementById('grid-container').style.display = "none";
+    document.getElementById('reset-button').style.display = "none";
+    console.log("Hidden");
+}
+
 function showBoard(){
     document.getElementById('insturctions').style.display = "none";
     document.getElementById('play-button').style.display = "none";
     document.getElementById('grid-container').style.display = "block";
     document.getElementById('reset-button').style.display = "block";
     console.log("Change");
+}
+
+//document.getElementsByClassName('grid').addEventListener('click', 
+function changeValue (id) {
+    console.log(id);
+    if(player == 1){
+        document.getElementById(id).innerHTML = "X";
+        player = 2;
+    }else if(player == 2){
+        document.getElementById(id).innerHTML = "O";
+        player = 1;
+    }
 }
