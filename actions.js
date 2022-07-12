570c580ac58c91ccbd8d53c800b53f633ee4aa31
luisgallegos23 <82685610+luisgallegos23@users.noreply.github.com>
2022-07-11 21:40:10 -0500

    Created board array and update function

--- actions.js
+++ actions.js
@@ -1,37 +1,80 @@
 var player = 1;
+var board = [
+    ['-','-','-'],
+    ['-','-','-'],
+    ['-','-','-']
+];
 
+//Hides the board upon the window loading 
 window.onload = function (){
     document.getElementById('grid-container').style.display = "none";
     document.getElementById('reset-button').style.display = "none";
     console.log("Hidden");
 }
 
+//hides the instructions and shows the board 
 function showBoard(){
     document.getElementById('insturctions').style.display = "none";
     document.getElementById('play-button').style.display = "none";
     document.getElementById('grid-container').style.display = "block";
     document.getElementById('reset-button').style.display = "block";
-    console.log("Change");
 }
 
-//document.getElementsByClassName('grid').addEventListener('click', 
+//Makes the change to the inner text of the grid
 function changeValue (id) {
-    console.log(id);
-    if(player == 1 && validClicked(id) ){
+    if(player == 1 && validClick(id) ){
         document.getElementById(id).innerHTML = "X";
-        player = 2;
-    }else if(player == 2 && validClicked(id) ){
+        updateBoard(id, 'X');
+        player = 2; //changes the current player
+    }else if(player == 2 && validClick(id) ){
         document.getElementById(id).innerHTML = "O";
-        player = 1;
+        updateBoard(id,'O');
+        player = 1; //changes player
     }
+    console.log(board.toString());
 }
 
-function validClicked(id){
+//Checks wheather the move is valid or not
+//prevents the symbole from being changed after one click
+function validClick(id){
     var valid = document.getElementById(id).innerHTML;
-    console.log(valid)
-    if(valid !== " - "){
+    if(valid !== " - "){ //inner text 
         return false;
     }else{
         return true;
     }
+}
+
+//updates the array board 
+function updateBoard (id, gridsymbol) {
+    switch (id) {
+    case 'b1': 
+        board[0][0] = gridsymbol;
+        break;
+    case 'b2': 
+        board[0][1] = gridsymbol;
+        break; 
+    case 'b3': 
+        board[0][2] = gridsymbol;
+        break;
+    case 'b4':
+        board[1][0] = gridsymbol;
+        break;
+    case 'b5':
+        board[1][1] = gridsymbol;
+        break;
+    case 'b6':
+        board[1][2] = gridsymbol;
+        break;
+    case 'b7':
+        board[2][0] = gridsymbol;
+        break;
+    case 'b8':
+        board[2][1] = gridsymbol;
+        break;
+    case 'b9':
+        board[2][2] = gridsymbol;
+        break;       
+               
+}
 }
